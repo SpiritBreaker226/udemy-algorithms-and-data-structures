198442042f28233256ea8ecf4057168164813aa8
Jason Stathopulos <jstathopulos@gmail.com>
2020-05-11 17:03:11 -0400

    Add Instructor Solution For Freq Counter Challenge
    Add Instructor Solution to compare for frequency counter challenge.

--- snippets/problem-solving/frequency-counter-challenge-anagrams.js
+++ snippets/problem-solving/frequency-counter-challenge-anagrams.js
@@ -42,3 +42,32 @@ console.log(
   'texttwisttime example',
   validAnagram('texttwisttime', 'timetwisttext')
 ) // ecpect: true
+
+/*
+// Instructor solution to compare
+function validAnagram(first, second) {
+  if (first.length !== second.length) {
+    return false
+  }
+
+  const lookup = {}
+
+  for (let index = 0; index < first.length; index++) {
+    let letter = first[index]
+    // fi letter exists, increment, otherwise set to 1
+    lookup[letter] ? (lookup[letter] += 1) : (lookup[letter] = 1)
+  }
+
+  for (let index = 0; index < second.length; index++) {
+    let letter = second[index]
+    // can't find letter or letter is zero then it's not an anagram
+    if (!lookup[letter]) {
+      return false
+    } else {
+      lookup[letter] -= 1
+    }
+  }
+
+  return true
+}
+*/
