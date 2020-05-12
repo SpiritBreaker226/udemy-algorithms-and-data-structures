7a5135cd584081ef5c3a00d6627fb4c201ca8afa
Jason Stathopulos <jstathopulos@gmail.com>
2020-05-12 18:31:28 -0400

    Add Instructor Solution, Average Pair Challenge
    Add Instructor Solution to compare for the Average Pair challenge.

--- problem-solving/multiple-pointers-challenge/average-pair.js
+++ problem-solving/multiple-pointers-challenge/average-pair.js
@@ -27,4 +27,22 @@ function averagePair(arr, num) {
   return false
 }
 
+/*
+// Instructor solution to compare
+function averagePair(arr, num){
+  let start = 0
+  let end = arr.length-1;
+
+  while(start < end){
+    let avg = (arr[start]+arr[end]) / 2
+
+    if(avg === num) return true;
+    else if(avg < num) start++
+    else end--
+  }
+
+  return false;
+}
+*/
+
 module.exports = averagePair
