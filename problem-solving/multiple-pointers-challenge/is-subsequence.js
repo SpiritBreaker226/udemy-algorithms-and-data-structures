3e6033b9fa8f52f776443f0a22928f05aee9df84
Jason Stathopulos <jstathopulos@gmail.com>
2020-05-12 18:34:32 -0400

    Add Instructor Solution, `isSubsequence` Challenge
    Add Instructor Solution to compare for the `isSubsequence` challenge.

--- problem-solving/multiple-pointers-challenge/is-subsequence.js
+++ problem-solving/multiple-pointers-challenge/is-subsequence.js
@@ -29,4 +29,29 @@ function isSubsequence(str, baseStr) {
   return false
 }
 
+/*
+// Instructor solution to compare
+
+// isSubsequence Solution - Iterative
+function isSubsequence(str1, str2) {
+  var i = 0;
+  var j = 0;
+  if (!str1) return true;
+  while (j < str2.length) {
+    if (str2[j] === str1[i]) i++;
+    if (i === str1.length) return true;
+    j++;
+  }
+  return false;
+}
+
+// isSubsequence Solution - Recursive but not O(1) Space
+function isSubsequence(str1, str2) {
+  if(str1.length === 0) return true
+  if(str2.length === 0) return false
+  if(str2[0] === str1[0]) return isSubsequence(str1.slice(1), str2.slice(1))
+  return isSubsequence(str1, str2.slice(1))
+}
+*/
+
 module.exports = isSubsequence
