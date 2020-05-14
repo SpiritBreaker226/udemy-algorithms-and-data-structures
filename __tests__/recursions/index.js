fb1ec75c4e95b16e75398f2a53f8d0a1e1ddc448
Jason Stathopulos <jstathopulos@gmail.com>
2020-05-14 17:02:30 -0400

    Add Factorial Challenge For Recursion
    Add the factorial challenge for the Recursion section to have more
    exercises to use the Recursion pattern.

--- __tests__/recursions/index.js
+++ __tests__/recursions/index.js
@@ -1,4 +1,5 @@
 const power = require('../../recursions/challenges/power')
+const factorial = require('../../recursions/challenges/factorial')
 
 describe('recursions', () => {
   describe('for challenge power', () => {
@@ -12,6 +13,24 @@ describe('recursions', () => {
 
     it('should be 16 for 2 power of four', () => {
       expect(power(2, 4)).toEqual(16)
+    })
+  })
+
+  describe('for challenge factorial', () => {
+    it('should be 1 for factorial 1!', () => {
+      expect(factorial(1)).toEqual(1)
+    })
+
+    it('should be 2 for factorial 2!', () => {
+      expect(factorial(2)).toEqual(2)
+    })
+
+    it('should be 24 for factorial 4!', () => {
+      expect(factorial(4)).toEqual(24)
+    })
+
+    it('should be 5040 for factorial 7!', () => {
+      expect(factorial(7)).toEqual(5040)
     })
   })
 })
---
+++ recursions/challenges/factorial.js
@@ -0,0 +1,16 @@
+// ===============================================================
+// Write a function factorial which accepts a number and returns
+// the factorial of that number. A factorial is the product of an
+// integer and all the intergers below it: e.g. factorial (4!) is
+// equal to 24, because 4 * 3 * 2 * 1 equals 24. factorial
+// zero (0!) is always 1.
+// ===============================================================
+
+function factorial(num) {
+  if (num < 0) return 0
+  if (num <= 1) return 1
+
+  return num * factorial(num - 1)
+}
+
+module.exports = factorial
