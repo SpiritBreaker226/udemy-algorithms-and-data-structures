a442828df4ff2a66e751c8e8a655f34540b4e5c4
Jason Stathopulos <jstathopulos@gmail.com>
2020-05-14 15:02:43 -0400

    Add Recursion to Factorial Recusion Example
    Add the recursion part to factorial recursion example to show that the
    recursion is a little bit better the using loops.

--- recursions/factorial.js
+++ recursions/factorial.js
@@ -8,4 +8,24 @@ function factorialNonRecusion(num) {
   return total
 }
 
+function factorialRecusion(num) {
+  if (num === 1) return 1
+
+  return num * factorialRecusion(num - 1)
+}
+
+let t1 = performance.now()
+
 console.log(factorialNonRecusion(4))
+
+let t2 = performance.now()
+
+console.log(`Non-Recusion Performance: ${(t2 - t1) / 1000} seconds.`)
+
+t1 = performance.now()
+
+console.log(factorialRecusion(4))
+
+t2 = performance.now()
+
+console.log(`Recusion Performance: ${(t2 - t1) / 1000} seconds.`)
