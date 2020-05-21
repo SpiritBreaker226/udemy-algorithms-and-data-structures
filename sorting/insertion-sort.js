2850dbfa86e74003948255038a2de88a852a175f
Jason Stathopulos <jstathopulos@gmail.com>
2020-05-21 19:05:19 -0400

    Add Instructor Solution to Insertion Sort Example
    Add Instructor Solution to compare for the Insertion Sort Example.

--- sorting/insertion-sort.js
+++ sorting/insertion-sort.js
@@ -5,34 +5,54 @@
 
 // Time Complexity: O(n^2)
 
+// function insertionSort(numbers) {
+//   let newNumbers = [...numbers]
+
+//   for (let index = 1; index < newNumbers.length; index++) {
+//     let extractedValue = newNumbers.splice(index, 1)[0]
+//     let noAdded = true
+
+//     for (let innerIndex = index - 1; innerIndex >= 0; innerIndex--) {
+//       if (newNumbers[innerIndex] > extractedValue) {
+//         continue
+//       }
+
+//       noAdded = false
+//       newNumbers.splice(innerIndex + 1, 0, extractedValue)
+//       console.log('innerIndex', innerIndex)
+//       break
+//     }
+
+//     if (noAdded) {
+//       if (newNumbers[0] > extractedValue) {
+//         newNumbers.splice(0, 0, extractedValue)
+//       } else {
+//         newNumbers.splice(1, 0, extractedValue)
+//       }
+//     }
+//   }
+
+//   return newNumbers
+// }
+
+// Instructor solution to compare
 function insertionSort(numbers) {
-  let newNumbers = [...numbers]
+  for (let index = 1; index < numbers.length; index++) {
+    let innerIndex = index - 1
+    const currentVal = numbers[index]
 
-  for (let index = 1; index < newNumbers.length; index++) {
-    let extractedValue = newNumbers.splice(index, 1)[0]
-    let noAdded = true
-
-    for (let innerIndex = index - 1; innerIndex >= 0; innerIndex--) {
-      if (newNumbers[innerIndex] > extractedValue) {
-        continue
-      }
-
-      noAdded = false
-      newNumbers.splice(innerIndex + 1, 0, extractedValue)
-      console.log('innerIndex', innerIndex)
-      break
+    for (
+      innerIndex;
+      innerIndex >= 0 && numbers[innerIndex] > currentVal;
+      innerIndex--
+    ) {
+      numbers[innerIndex + 1] = numbers[innerIndex]
     }
 
-    if (noAdded) {
-      if (newNumbers[0] > extractedValue) {
-        newNumbers.splice(0, 0, extractedValue)
-      } else {
-        newNumbers.splice(1, 0, extractedValue)
-      }
-    }
+    numbers[innerIndex + 1] = currentVal
   }
 
-  return newNumbers
+  return numbers
 }
 
 module.exports = insertionSort
