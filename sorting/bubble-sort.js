bee772b848874718e146903e2e889added3e31d8
Jason Stathopulos <jstathopulos@gmail.com>
2020-05-21 10:24:28 -0400

    Add Optimize Bubble Sort Version
    Add the optimize bubble sort version not to loop if no swap has happened
    to skip the rest of the array as the array has already sorted.

--- sorting/bubble-sort.js
+++ sorting/bubble-sort.js
@@ -7,12 +7,15 @@
 
 function bubbleSort(numbers) {
   let newNumbers = [...numbers]
+  let noSwap
 
   // goes around each number controlling the passes for the bubble sort
   // To prevent not having to sort the largest numbers again since they
   //  are already in place and would not move again and wasting the time
   // of those.
   for (let index = newNumbers.length; index > 0; index--) {
+    noSwap = true
+
     // goes around each of numbers in newNumbers
     // starting from thewhere the  previous index to make it
     // so that the loop does not need to go throw elemeents around sorted
@@ -21,8 +24,14 @@ function bubbleSort(numbers) {
       // if so then swap the positions around
       if (newNumbers[innerIndex] > newNumbers[innerIndex + 1]) {
         swap(newNumbers, innerIndex, innerIndex + 1)
+
+        noSwap = false
       }
     }
+
+    // checks if there has been a swap if not then stop looping as bubble sot
+    // is done
+    if (noSwap) break
   }
 
   return newNumbers
