a503c3c5fc74dcedf43d25ff6b30e3196b58d4af
Jason Stathopulos <jstathopulos@gmail.com>
2020-05-27 10:08:13 -0400

    Fix All 'Craze' Sort Tests in the Insertion Sort
    Fix all of the 'Craze' sort algorithm tests from being in the insertion
    sort test. So that when the test is output on the terminal, they are on
    the same level as insertion sort, which they are supposed to be.

--- __tests__/sorting/index.js
+++ __tests__/sorting/index.js
@@ -93,163 +93,193 @@ describe('sorting', () => {
         100,
       ])
     })
+  })
 
-    describe('for merge sort', () => {
-      it('should return a sorted short array', () => {
-        expect(mergeSort([29, 6, 54, 39, 28, 19, 25])).toEqual([
-          6,
-          19,
-          25,
-          28,
-          29,
-          39,
-          54,
-        ])
+  describe('for merge sort', () => {
+    it('should return a sorted short array', () => {
+      expect(mergeSort([29, 6, 54, 39, 28, 19, 25])).toEqual([
+        6,
+        19,
+        25,
+        28,
+        29,
+        39,
+        54,
+      ])
+    })
+
+    it('should return a sorted large array', () => {
+      expect(mergeSort([100, 50, 82, 69, 25, 77, 99, 32, 75, 45, 10])).toEqual([
+        10,
+        25,
+        32,
+        45,
+        50,
+        69,
+        75,
+        77,
+        82,
+        99,
+        100,
+      ])
+    })
+
+    describe('on merging', () => {
+      describe('when the right side array is larger', () => {
+        it('should return a sorted array', () => {
+          expect(merge([1, 3, 50], [5, 42, 99, 100])).toEqual([
+            1,
+            3,
+            5,
+            42,
+            50,
+            99,
+            100,
+          ])
+        })
       })
 
-      it('should return a sorted large array', () => {
-        expect(
-          mergeSort([100, 50, 82, 69, 25, 77, 99, 32, 75, 45, 10])
-        ).toEqual([10, 25, 32, 45, 50, 69, 75, 77, 82, 99, 100])
+      describe('when the left side array is larger', () => {
+        it('should return a sorted array', () => {
+          expect(merge([1, 3, 20, 42, 50, 89], [5, 42, 99, 100])).toEqual([
+            1,
+            3,
+            5,
+            20,
+            42,
+            42,
+            50,
+            89,
+            99,
+            100,
+          ])
+        })
       })
 
-      describe('on merging', () => {
-        describe('when the right side array is larger', () => {
-          it('should return a sorted array', () => {
-            expect(merge([1, 3, 50], [5, 42, 99, 100])).toEqual([
-              1,
-              3,
-              5,
-              42,
-              50,
-              99,
-              100,
-            ])
-          })
-        })
-
-        describe('when the left side array is larger', () => {
-          it('should return a sorted array', () => {
-            expect(merge([1, 3, 20, 42, 50, 89], [5, 42, 99, 100])).toEqual([
-              1,
-              3,
-              5,
-              20,
-              42,
-              42,
-              50,
-              89,
-              99,
-              100,
-            ])
-          })
-        })
-
-        describe('when both arrays are equal', () => {
-          it('should return a sorted array', () => {
-            expect(merge([1, 3, 42, 50], [5, 42, 99, 100])).toEqual([
-              1,
-              3,
-              5,
-              42,
-              42,
-              50,
-              99,
-              100,
-            ])
-          })
+      describe('when both arrays are equal', () => {
+        it('should return a sorted array', () => {
+          expect(merge([1, 3, 42, 50], [5, 42, 99, 100])).toEqual([
+            1,
+            3,
+            5,
+            42,
+            42,
+            50,
+            99,
+            100,
+          ])
         })
       })
     })
+  })
 
-    describe('for quick sort', () => {
-      it('should return a sorted short array', () => {
-        expect(quickSort([29, 6, 54, 39, 28, 19, 25])).toEqual([
-          6,
-          19,
-          25,
-          28,
-          29,
-          39,
-          54,
-        ])
+  describe('for quick sort', () => {
+    it('should return a sorted short array', () => {
+      expect(quickSort([29, 6, 54, 39, 28, 19, 25])).toEqual([
+        6,
+        19,
+        25,
+        28,
+        29,
+        39,
+        54,
+      ])
+    })
+
+    it('should return a sorted large array', () => {
+      expect(quickSort([100, 50, 82, 69, 25, 77, 99, 32, 75, 45, 10])).toEqual([
+        10,
+        25,
+        32,
+        45,
+        50,
+        69,
+        75,
+        77,
+        82,
+        99,
+        100,
+      ])
+    })
+
+    describe('on pivot helper', () => {
+      it('should return index of pivot', () => {
+        const numbers = [45, 50, 82, 25, 77, 32, 75, 10]
+
+        expect(pivotHelper(numbers)).toEqual(3)
+        expect(numbers).toEqual([10, 25, 32, 45, 77, 82, 75, 50])
+      })
+    })
+  })
+
+  describe('for radix sort', () => {
+    it('should return a sorted short array', () => {
+      expect(radixSort([29, 6, 54, 39, 28, 19, 25])).toEqual([
+        6,
+        19,
+        25,
+        28,
+        29,
+        39,
+        54,
+      ])
+    })
+
+    it('should return a sorted large array', () => {
+      expect(radixSort([100, 50, 82, 69, 25, 77, 99, 32, 75, 45, 10])).toEqual([
+        10,
+        25,
+        32,
+        45,
+        50,
+        69,
+        75,
+        77,
+        82,
+        99,
+        100,
+      ])
+    })
+
+    describe('on getDigit', () => {
+      it('should return zero when place does not exist', () => {
+        expect(getDigit(1, 2)).toEqual(0)
       })
 
-      it('should return a sorted large array', () => {
-        expect(
-          quickSort([100, 50, 82, 69, 25, 77, 99, 32, 75, 45, 10])
-        ).toEqual([10, 25, 32, 45, 50, 69, 75, 77, 82, 99, 100])
-      })
-
-      describe('on pivot helper', () => {
-        it('should return index of pivot', () => {
-          const numbers = [45, 50, 82, 25, 77, 32, 75, 10]
-
-          expect(pivotHelper(numbers)).toEqual(3)
-          expect(numbers).toEqual([10, 25, 32, 45, 77, 82, 75, 50])
-        })
+      it('should return right side digit', () => {
+        expect(getDigit(12345, 1)).toEqual(4)
       })
     })
 
-    describe('for radix sort', () => {
-      it('should return a sorted short array', () => {
-        expect(radixSort([29, 6, 54, 39, 28, 19, 25])).toEqual([
-          6,
-          19,
-          25,
-          28,
-          29,
-          39,
-          54,
-        ])
+    describe('on digitCount', () => {
+      it('should return 1 on zero', () => {
+        expect(digitCount(0)).toEqual(1)
       })
 
-      it('should return a sorted large array', () => {
-        expect(
-          radixSort([100, 50, 82, 69, 25, 77, 99, 32, 75, 45, 10])
-        ).toEqual([10, 25, 32, 45, 50, 69, 75, 77, 82, 99, 100])
+      it('should return 1', () => {
+        expect(digitCount(1)).toEqual(1)
       })
 
-      describe('on getDigit', () => {
-        it('should return zero when place does not exist', () => {
-          expect(getDigit(1, 2)).toEqual(0)
-        })
-
-        it('should return right side digit', () => {
-          expect(getDigit(12345, 1)).toEqual(4)
-        })
+      it('should return 25', () => {
+        expect(digitCount(25)).toEqual(2)
       })
 
-      describe('on digitCount', () => {
-        it('should return 1 on zero', () => {
-          expect(digitCount(0)).toEqual(1)
-        })
+      it('should return 1234', () => {
+        expect(digitCount(1234)).toEqual(4)
+      })
+    })
 
-        it('should return 1', () => {
-          expect(digitCount(1)).toEqual(1)
-        })
-
-        it('should return 25', () => {
-          expect(digitCount(25)).toEqual(2)
-        })
-
-        it('should return 1234', () => {
-          expect(digitCount(1234)).toEqual(4)
-        })
+    describe('on mostDigits', () => {
+      it('should return 4', () => {
+        expect(mostDigits([1234, 56, 7])).toEqual(4)
       })
 
-      describe('on mostDigits', () => {
-        it('should return 4', () => {
-          expect(mostDigits([1234, 56, 7])).toEqual(4)
-        })
+      it('should return 5', () => {
+        expect(mostDigits([1, 11111, 1])).toEqual(5)
+      })
 
-        it('should return 5', () => {
-          expect(mostDigits([1, 11111, 1])).toEqual(5)
-        })
-
-        it('should return 2', () => {
-          expect(mostDigits([12, 34, 56, 78])).toEqual(2)
-        })
+      it('should return 2', () => {
+        expect(mostDigits([12, 34, 56, 78])).toEqual(2)
       })
     })
   })
