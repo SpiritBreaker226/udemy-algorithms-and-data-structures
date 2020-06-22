9597f94793e475c423833058400f050bb99f993a
Jason Stathopulos <jstathopulos@gmail.com>
2020-06-22 11:38:28 -0400

    Add Another Way to do Next Fibonacci Number
    Another Way to do Fibonacci by updating a two-element array with the
    next Fibonacci numbers, thus reducing the space complexity O(1) since
    the array changes in place, and there is no way of memory to find the
    number of Fibonacci sequences. Also, no, this is zero bases. At the same
    time, the current version is one base.
    
    ---
    
    Refer:
    
    AlgoExpert: https://www.algoexpert.io/questions/Nth%20Fibonacci
    	- subscription is required

--- recursions/challenges/fib.js
+++ recursions/challenges/fib.js
@@ -19,4 +19,29 @@ function fib(num) {
   return cache[num]
 }
 
+// Another Way to do fibonacci by updating a two element array with the next
+// Fibonacci numbers thus reducing the space complexity O(1) since the array is
+// change in place and there is no wate of mememory to fine the number of
+// Fibonacci sequrence Also no this is zero base while othe current version is
+// one base
+
+// Refer: AlgoExpert - https://www.algoexpert.io/questions/Nth%20Fibonacci
+// subscription is required
+
+// function fib(num) {
+//   const lastTwo = [0, 1]
+//   let counter = 3
+
+//   while (counter <= num) {
+//     let nextFib = lastTwo[0] + lastTwo[1]
+
+//     lastTwo[0] = lastTwo[1]
+//     lastTwo[1] = nextFib
+
+//     counter++
+//   }
+
+//   return (num > 1) ? lastTwo[1] : lastTwo[0]
+// }
+
 module.exports = fib
