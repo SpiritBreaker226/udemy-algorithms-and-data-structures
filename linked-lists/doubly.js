e7fa25a911ec3845038d56d698046e91866bdc39
Jason Stathopulos <jstathopulos@gmail.com>
2020-06-30 16:01:13 -0400

    Add Push Function to DoublyLinkedList Class
    Add the Push function to the DoublyLinkedList class to update the tail
    to point to a new node when the developer wants to add a node to the end
    of the list. Also, updates the previous to point to the old tail so that
    the developer can go back to the original tail.

--- linked-lists/doubly.js
+++ linked-lists/doubly.js
@@ -16,4 +16,21 @@ class DoublyLinkedList {
     this.tail = null
     this.length = 0
   }
+
+  push(value) {
+    const node = new Node(value)
+
+    if (this.length === 0) {
+      this.head = node
+      this.tail = node
+    } else {
+      this.tail.next = node
+      node.prev = this.tail
+      this.tail = node
+    }
+
+    this.length++
+
+    return this
+  }
 }
