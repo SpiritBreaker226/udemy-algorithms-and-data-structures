17038b75ccde5185361c3f0ca8ead60c11073513
Jason Stathopulos <jstathopulos@gmail.com>
2020-07-02 18:28:59 -0400

    Add Instructor Solution For Insert in BST Class
    Add Instructor solution to compare for Insert in Binary Search Tree
    class.

--- trees/binary-search-tree.js
+++ trees/binary-search-tree.js
@@ -32,6 +32,39 @@ class BinarySearchTree {
 
     return this
   }
+
+  // Instructor solution to compare
+  // insert(value) {
+  //   const newNode = new Node(value)
+
+  //   if (!this.root) {
+  //     this.root = newNode
+
+  //     return this
+  //   }
+
+  //   let current = this.root
+
+  //   while (true) {
+  //     if (value === current.value) {
+  //       return undefined
+  //     } else if (value < current.value) {
+  //       if (!current.left) {
+  //         current.left = new Node(value)
+  //         return this
+  //       }
+
+  //       current = current.left
+  //     } else {
+  //       if (!current.right) {
+  //         current.right = new Node(value)
+  //         return this
+  //       }
+
+  //       current = current.right
+  //     }
+  //   }
+  // }
 }
 
 module.exports = BinarySearchTree
