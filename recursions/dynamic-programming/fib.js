b33fad0b3c0261ae3db3fed620ebaa0bc2063b0d
Jason Stathopulos <jstathopulos@gmail.com>
2020-07-15 16:17:47 -0400

    Add Tabulation to Fibonacci Dynamic Programming
    Add tabulation to Fibonacci Sequence in Dynamic Programming example to
    show restart from simple problems and work our way up to more complex
    issues, which can save both in time and space complexities.

--- recursions/dynamic-programming/fib.js
+++ recursions/dynamic-programming/fib.js
@@ -1,11 +1,27 @@
-const cache = []
+// Memoization
+
+// const cache = []
+
+// function fib(num) {
+//   if (num <= 2) return 1
+
+//   if (!cache[num]) cache[num] = fib(num - 1) + fib(num - 2)
+
+//   return cache[num]
+// }
+
+// Tabulation
 
 function fib(num) {
   if (num <= 2) return 1
 
-  if (!cache[num]) cache[num] = fib(num - 1) + fib(num - 2)
+  const fibNumbers = [0, 1, 1]
 
-  return cache[num]
+  for (let index = 3; index <= num; index++) {
+    fibNumbers[index] = fibNumbers[index - 1] + fibNumbers[index - 2]
+  }
+
+  return fibNumbers[num]
 }
 
 module.exports = fib
